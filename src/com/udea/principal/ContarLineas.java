54eca28bdd376c29abb8e8d408bc234a46d6a48a
Orozco <luisfernandorzcrzc@gmail.com>
2017-08-22 19:59:55 -0500

    pruebas contarLineas

--- src/com/udea/principal/ContarLineas.java
+++ src/com/udea/principal/ContarLineas.java
@@ -1,8 +1,35 @@
 package com.udea.principal;
 
 import java.util.List;
+
 public class ContarLineas {
 	public static int ContarLineas(List<String> programa){
-		return 0;
+		int contador=0;
+		if(programa==null){
+			return 0;
+		}
+		if(programa.size()==0){
+			return 0;
+		}
+		
+		for (String linea : programa) {
+			String sublineas[] = linea.split(";");
+			if(sublineas.length>1){
+				
+			}else{
+				if(linea.contains("for")){
+					contador+=3;
+				}else if(linea.length()==1 && (linea.equals("{") || linea.equals("}"))){
+					
+				}else{
+					contador++;
+				}
+			}
+			
+			
+			
+		}
+		
+		return contador;
 	}
 }

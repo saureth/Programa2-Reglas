627eecb34b32b7c0cb3e1a76ddf9cfe6edbd41ed
Saureth <aerials_001@hotmail.com>
2017-09-04 12:45:11 -0500

    Continuo haciendo el contador de clases

---
+++ src/com/udea/principal/ContarClases.java
@@ -0,0 +1,9 @@
+package com.udea.principal;
+
+public class ContarClases {
+
+	public int Contar(String string) {
+		return -1;
+	}	
+	
+}
---
+++ test/com/udea/RevisorTest/TestContarClases.java
@@ -0,0 +1,27 @@
+package com.udea.RevisorTest;
+import static org.junit.Assert.assertTrue;
+import java.util.List;
+import org.junit.Test;
+
+import com.udea.principal.ContarClases;
+import com.udea.principal.LectorArchivos;
+
+public class TestContarClases {
+
+	@Test
+	public void TestNulo() 
+	{
+		ContarClases ca = new ContarClases();
+		int cantidad = ca.Contar("");
+		assertTrue(cantidad == -1);
+	}
+	
+	@Test
+	public void TestNoEncontrado() 
+	{
+		ContarClases ca = new ContarClases();
+		int cantidad = ca.Contar("../../../../../../asdasdasasd");
+		assertTrue(cantidad == -1);
+	}
+	
+}
